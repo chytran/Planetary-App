2f8b80dd52241641be5c19e2f76419433cf0ea06
chytran <chytran@users.noreply.github.com>
2021-06-10 12:43:19 -0700

    JSON

--- js/main.js
+++ js/main.js
@@ -2,30 +2,22 @@ const info = {
     link: "http://api.open-notify.org/astros.json"
 }
 
+
 const planetContainer = document.querySelector('.planet__container');
 
-const planets = document.createElement("div");
-planets.className = 'planet';
-
 // get info from api
-fetch(info.link, {
-    method: 'POST',
-    body: {
-
-    }
-})
-    .then(res => {
-        if (res.ok){
-            console.log('SUCCESS')
-        } else {
-            console.log("Not Successful")
-        }
-        res.json()
-    })
+fetch(info.link)
+    .then(res => res.json())
     .then(data => {
-        planets.innerHTML = `
-            <h2></h2>
+        let list = document.createElement("li");
+        list.innerHTML = `
+            ${data.people.forEach(e => console.log(e))}
         `;
+
+        planetContainer.innerHTML = `
+            <h2 class="planet__title">${data.number} are currently in space</h2>
+            ${data.people.forEach(e => console.log(e))}
+        `;
+
         console.log(data)
-    })
-    .catch(error => console.log('ERROR'));
+    });

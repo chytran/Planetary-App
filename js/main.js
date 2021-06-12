a38dfe39a3b23d28d0390ae22b0adfe884732241
chytran <chytran@users.noreply.github.com>
2021-06-12 12:05:56 -0700

    Adding to main branch

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const info = {
+    link: "http://api.open-notify.org/astros.json"
+}
+
+
+const planetContainer = document.querySelector('.planet__container');
+
+// get info from api
+fetch(info.link)
+    .then(res => res.json())
+    .then(data => {
+        planetContainer.innerHTML = `
+            <h2 class="planet__title">${data.number} people are currently in space</h2>
+            <ul class="list">
+                <li class="items">${data.people[0].name}</li>
+                <li class="items">${data.people[1].name}</li>
+                <li class="items">${data.people[2].name}</li>
+                <li class="items">${data.people[3].name}</li>
+                <li class="items">${data.people[4].name}</li>
+                <li class="items">${data.people[5].name}</li>
+                <li class="items">${data.people[6].name}</li>
+            </ul>
+
+        `;
+
+        console.log(data)
+    });
